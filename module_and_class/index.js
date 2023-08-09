f84761aa5fe47f135d9041ef694957f3b41f4b8d
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-08-10 02:49:38 +0600

    export and import

---
+++ module_and_class/index.js
@@ -0,0 +1,8 @@
+//import { text, num } from "./index2.js"; //named import
+
+// console.log(text);
+// console.log(num);
+
+import * as test from "./index2.js";
+test.myfunc("I love Bangladesh");
+console.log(test.text);
---
+++ module_and_class/index2.js
@@ -0,0 +1,6 @@
+export let text = "I love JS";
+export let num = 2023;
+
+export const myfunc = (txt) => {
+  text = txt;
+};
